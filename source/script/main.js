e72375c4d4af818c63f3ac1b174c98edf75da946
Hennady95 <55188385+Hennady95@users.noreply.github.com>
2021-01-12 00:11:37 +0300

    Add files via upload

---
+++ source/script/main.js
@@ -0,0 +1,71 @@
+import { getMathFunction, getResult, getPercent } from './mathFunction.js';
+
+//переменные операций
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+let division = document.getElementById('division');
+let multiplication = document.getElementById('multiplication');
+let percent = document.getElementById('percent');
+let plusMin = document.getElementById('plusMin');
+let clear = document.getElementById('clear');
+let dot = document.getElementById('dot');
+let result = document.getElementById('result');
+
+let enterNumber = document.querySelector('.calculate__display--number');
+
+let showOperation = document.querySelector('.calculate__display--operation');
+
+let showResult = document.querySelector('.calculate__display--result');
+
+let buttonPanel = document.querySelector('.calculate__body');
+
+let number = 0;
+let secondNumber = 0;
+let operation = '';
+
+buttonPanel.addEventListener('click', (event) => {
+    if(event.target.classList.contains('button--number')) {
+     enterNumber.textContent += event.target.textContent;
+    } else if(event.target.id === 'clear') { 
+        number = 0;
+        operation = '';
+        enterNumber.textContent = '';
+        showOperation.textContent = '';
+    } else if(event.target.id === 'plus'  && enterNumber.textContent.length != 0 ) {
+        number = getMathFunction(operation, number, +enterNumber.textContent)
+        showOperation.textContent += `${number} + `;
+        operation = 'plus';
+        enterNumber.textContent = '';
+    } else if(event.target.id === 'minus' && enterNumber.textContent.length != 0 ) {
+        number = getMathFunction(operation, number, +enterNumber.textContent)
+        showOperation.textContent += `${number} - `;
+        operation = 'minus';
+        enterNumber.textContent = '';
+    } else if(event.target.id === 'multiplication'  && enterNumber.textContent.length != 0 ) {  
+        number = getMathFunction(operation, number, +enterNumber.textContent);//getMultAndDivision(operation, 'x');
+        showOperation.textContent += `${number} x `;
+        operation = 'multiplication';
+        enterNumber.textContent = '';
+    } else if(event.target.id === 'division'  && enterNumber.textContent.length != 0 ) {
+        number = getMathFunction(operation, number, +enterNumber.textContent)//getMultAndDivision(operation, '/')
+        showOperation.textContent += `${number} / `;
+        operation = 'division';
+        enterNumber.textContent = '';
+    } else if(event.target.id === 'percent') {
+        console.log(number);
+        number = getPercent(operation, number, +enterNumber.textContent);
+        operation = 'percent';
+        showOperation.textContent += `${number} % `;
+        enterNumber.textContent = ''; 
+    } else if(event.target.id === 'plusMin') { 
+        enterNumber.textContent =  +enterNumber.textContent * (-1);
+    } else if(event.target.id === 'result') {
+        number = getResult(operation, number, +enterNumber.textContent);
+        showResult.textContent = number;
+        showOperation.textContent = '';
+        enterNumber.textContent = '';
+        number = 0;
+        secondNumber = 0;
+        operation = '';
+    } 
+});
--- source/script/mathFunction.js
+++ source/script/mathFunction.js
@@ -1,76 +1,47 @@
-import {showOperation, enterNumber, number, secondNumber, showResult} from './script.js';
-
-function getOperation(operation, symbol, number, secondNumber) {
+function getMathFunction(operation, firstNumber, secondnumber) {
     if(operation === 'multiplication') {
-        secondNumber *= +enterNumber.textContent;
-        number = secondNumber;
+        firstNumber *= secondNumber;
     } else if(operation === 'division') {
-        secondNumber /= +enterNumber.textContent;
-        number = secondNumber;
+        firstNumber /= secondNumber;
     } else if(operation === 'minus') {
-        number -= +enterNumber.textContent;
+        firstNumber -= secondNumber;
     } else if(operation === 'plus') {
-        number += +enterNumber.textContent;
+        firstNumber += secondNumber;
     } else {
-        number = +enterNumber.textContent;
+        firstNumber = secondnumber;
     }
-    showOperation.textContent += `${enterNumber.textContent} ${symbol} `;
-    enterNumber.textContent = '';
+    return firstNumber;
 }
 
-function getMultAndDivision(operation, symbol) {
+function getPercent(operation, firstNumber, secondNumber) {
     if(operation === 'multiplication') {
-        secondNumber *= +enterNumber.textContent;
-    } else if(operation === 'division' ) {
-        secondNumber /= +enterNumber.textContent;
-    } else if(operation === 'minus' ) {
-        secondNumber -= +enterNumber.textContent;
-    } else if(operation === 'plus' ) {
-        secondNumber += number + +enterNumber.textContent;
-    } else {
-        secondNumber = +enterNumber.textContent;
-    }
-    showOperation.textContent += `${enterNumber.textContent} ${symbol} `;
-    enterNumber.textContent = '';
-}
-
-function getPercent(operation) {
-    console.log(number);
-    if(operation === 'multiplication') {
-        number = secondNumber * (+enterNumber.textContent / 100);
+        firstNumber *= (secondNumber / 100);
     } else if(operation === 'division'){
-         number = secondNumber / (+enterNumber.textContent / 100);
+        firstNumber /= (secondNumber / 100);
     } else if(operation === 'plus'){
-         number += number / 100 * +enterNumber.textContent ;
+        firstNumber += firstNumber / 100 * secondNumber ;
     } else if(operation === 'minus'){
-        number -= number / 100 * +enterNumber.textContent;
+        firstNumber -= firstNumber / 100 * secondNumber;
     } else {
-        number = +enterNumber.textContent / 100;
+        firstNumber = secondNumber / 100;
     }
-    number = +number.toFixed(2);
-    showOperation.textContent += `${enterNumber.textContent} % `;
-    enterNumber.textContent = ''; 
+    return +firstNumber.toFixed(2);
 }
 
-function getResult(operation) {
-    if(enterNumber.textContent.length != 0 && operation === 'plus') {
-        number += +enterNumber.textContent;
-    } else if(enterNumber.textContent.length != 0 && operation === 'minus') {
-        number -= +enterNumber.textContent;
-    }  else if(enterNumber.textContent.length != 0 && operation === 'multiplication') {
-        number = secondNumber * +enterNumber.textContent;
-    } else if(enterNumber.textContent.length != 0 && operation === 'division') {
-        number = secondNumber / +enterNumber.textContent;
-    } else if(enterNumber.textContent.length != 0 && operation === 'percent') {
-        number = number / 100 * +enterNumber.textContent;
+function getResult(operation, firstNumber, secondNumber) {
+    if(operation === 'plus') {
+        firstNumber += secondNumber;
+    } else if(operation === 'minus') {
+        firstNumber -= secondNumber;
+    }  else if(operation === 'multiplication') {
+        firstNumber  *= secondNumber;
+    } else if(operation === 'division') {
+        firstNumber /= secondNumber;
+    } else if(operation === 'percent') {
+        firstNumber = firstNumber ;
     }
-    number = +number.toFixed(2); 
-    showResult.textContent = number;
-    showOperation.textContent = '';
-    enterNumber.textContent = '';
-    number = 0;
-    secondNumber = 0;
-    operation = '';
+    return +firstNumber.toFixed(2); 
+    
 }
 
-export {getOperation, getMultAndDivision, getPercent, getResult};
+export {getMathFunction, getPercent, getResult};
